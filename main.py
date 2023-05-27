d6c23c6c45aba5606616f86d8798d6f31315ac2e
Max Hager <maxhager28@gmail.com>
2023-05-27 16:49:34 -0700

    removed main

--- main.py
+++
@@ -1,3 +0,0 @@
-from midjourney_api import MidjourneyApi
-
-midjourney = MidjourneyApi(prompt="Yoda", application_id="application_id", guild_id="guild_id", channel_id="channel_id", version="version", id="id", authorization="authorization")
